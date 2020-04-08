da9a85578eed706d7dac7934d1a839f8040e39de
Esapat <samrickky@gmail.com>
2020-04-09 02:23:53 +0300

    Assessment

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,24 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable no-undef */
+const covid19ImpactEstimator = (repostedCases) => ({
+  impact: {
+    currentlyInfected: repostedCases * 10
+  },
+
+  severeImpact: {
+    currentlyInfected: repostedCases * 50
+  },
+
+  infectionsByRequestedTime: {
+
+    impact: {
+      currentlyInfected: currentlyInfected * 1024
+    },
+    severeImpact: {
+      currentlyInfected: currentlyInfected * 1024
+    }
+  }
+
+});
+
 
 export default covid19ImpactEstimator;
