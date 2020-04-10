e931492b45b75698cbd0d39df79b97636890a87a
Esapat <samrickky@gmail.com>
2020-04-10 05:51:41 +0300

    Assessment

--- src/estimator.js
+++ src/estimator.js
@@ -1,33 +1,16 @@
 /* eslint-disable no-undef */
-const covid19ImpactEstimator = () => {
-  const result = [];
-  const data = [{
-
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  return {
+    data: input,
+    impact: {
+      currentlyinfected: data.reportedCases * 10,
+      infectionsByRequestedTime: currentlyinfected * 1024
     },
-    periodType: 'days',
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-  }];
-  for (let x = 0; x < data.length; x += 1) {
-    result.push({
-      data,
-      Impact: [{
-        currentlyInfected: data[x].reportedCases * 20
-      }],
-      severeImpact: [{
-        currentlyInfected: data[x].reportedCases * 50
-      }]
-    });
-  }
-  const arr = { ...result };
-  return arr;
+    severeImpact: {
+      currentlyinfected: data.reportedCases * 50
+    }
+  };
 };
 
 export default covid19ImpactEstimator;
